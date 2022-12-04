3e313545d90f97c620405b53f084dd36862cd336
KellerMarika <marikakeller92@gmail.com>
2022-12-04 02:15:40 +0100

    base

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,21 @@
+<!-------------
+  APP TITLE 
+-------------->
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: "La mia prima app con Vite!",
+    };
+  }
+};
+</script>
+
+<style>
+</style>
